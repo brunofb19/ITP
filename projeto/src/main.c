19e372f3c21e1ba050f5f5ca4bcceb0b1f4c0e9b
brunofb19 <brenoouhd@gmail.com>
2025-09-22 10:06:12 -0300

    add operadores basicos

--- projeto/src/main.c
+++ projeto/src/main.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <math.h>
+
+float calcular(char si, float a, float b){
+    float res_aux;
+    if(si=='+'){
+        res_aux=a+b;
+    }   else if (si=='-'){
+        res_aux=a-b;
+    }   else if (si=='/'){
+        res_aux=a/b;
+    }   else if (si=='*'){
+        res_aux=a*b;
+    }
+    return res_aux;
+}
+
+
 int main() {
- printf("hello world");
- return 0;
+    float n1, n2, res;
+    char op;
+
+    printf("ola, esta eh sua calculadora multi funcao\n");
+    printf("operadores suportados: +, -, /, *\n");
+
+    scanf("%f", &n1);
+    scanf(" %c", &op);
+    res=n1;
+
+    while (op!='.'){
+        scanf("%f", &n2);
+        res=calcular(op, res, n2);
+        scanf(" %c", &op);
+    }
+    printf("resultado da operacao: %f", res);
+
+
+    return 0;
 }
